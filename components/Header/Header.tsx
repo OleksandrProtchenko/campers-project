35daaebd700a6850d185f6bd55c6edd08746e824
OleksandrProtchenko <alexdevelop92@gmail.com>
2026-04-14 19:10:06 +0300

    add Header/home page markup and styles

---
+++ components/Header/Header.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import css from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={css.header}>
+      <div className={css.container}>
+        <Image
+          className={css.logo}
+          src="/Logo.png"
+          alt="Logo"
+          width={136}
+          height={16}
+        />
+        <nav className={css.nav}>
+          <ul className={css.navList}>
+            <li className={css.navItem}>
+              <Link className={css.navLink} href="/">
+                Home
+              </Link>
+            </li>
+            <li className={css.navItem}>
+              <Link className={css.navLink} href="/catalog">
+                Catalog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
